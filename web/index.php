f4496770d3156cd21c187e30b08e433ecfa1b9f1
cle61 <prodhomme.clement@hotmail.fr>
2015-10-23 15:37:29 +0200

    initial upload

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+$app = new Silex\Application();
+
+
+$app->get('/', function () {
+
+    return 'Hello world';
+
+});
+
+
+$app->run();
